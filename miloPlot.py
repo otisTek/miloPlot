f756e11a2ff66402e77806e9373008dff9c1d84a
Steve Paris -- Paris Aerospace Technologies <otisTek@gmail.com>
2017-02-01 14:38:57 -0800

    reshape / -> //

--- miloPlot.py
+++ miloPlot.py
@@ -340,7 +340,7 @@ while makePlot:
       xIndx=findIndex(xVar,allNames[j],fileList[j])
       yIndx=findIndex(yVar[0],allNames[j],fileList[j])
       data=np.array(allData[j])        
-      data=data.reshape((len(data)/len(allNames[j]),len(allNames[j])))
+      data=data.reshape((len(data)//len(allNames[j]),len(allNames[j])))
 # these are usually time points  (data[0,0],data[1,0],data[322,0])   
       if xIndx >= 0 and yIndx >=0: 
         ax.plot(data[:,xIndx],data[:,yIndx],label=dataLegends[j])
